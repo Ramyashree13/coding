ba848cd0d8a5dbdf08aa9658117c0e4b56935e1a
Ramyashree13 <47894457+Ramyashree13@users.noreply.github.com>
2019-07-23 11:20:09 +0530

    Update Breakingtherecord.c

--- Breakingtherecord.c
+++ Breakingtherecord.c
@@ -1,96 +1,28 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-vector<string> split_string(string);
-
-// Complete the breakingRecords function below.
 vector<int> breakingRecords(vector<int> scores) 
 {
-    int i,ele,l=0,s=0;
+    int i,ele,l=0,small=0,s,low;
     vector<int> res;
     //ele=scores[0];
-    for(i=0;i<scores.size();i++)
+    for(i=1;i<scores.size();i++)
     {
-        if(scores[i]>scores[i+1])
+        low=scores[0];
+        if(scores[i]>scores[i-1])
         {
             //ele=scores[i];
             l++;
+            s=scores[i-1];
         }
-        s=scores[i];
-        if(scores[i-1]!=s)
+        else
         {
-            s++;
+            s=scores[i];
+        }
+        if(low!=s || s<low)
+        {
+            low=s;
+            small++;
         }
     }
-    res.push_back(s);
+    res.push_back(small);
     res.push_back(l);
     return res;
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    int n;
-    cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    string scores_temp_temp;
-    getline(cin, scores_temp_temp);
-
-    vector<string> scores_temp = split_string(scores_temp_temp);
-
-    vector<int> scores(n);
-
-    for (int i = 0; i < n; i++) {
-        int scores_item = stoi(scores_temp[i]);
-
-        scores[i] = scores_item;
-    }
-
-    vector<int> result = breakingRecords(scores);
-
-    for (int i = 0; i < result.size(); i++) {
-        fout << result[i];
-
-        if (i != result.size() - 1) {
-            fout << " ";
-        }
-    }
-
-    fout << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
